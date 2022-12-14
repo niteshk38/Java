d8fd1bd16313090ccb6ca8712f1fd6f600a22f80
Nitesh Kumar <niteshk38@gmail.com>
2022-12-15 00:53:20 +0530

    Create Use of Getters and Setters in main method.java

---
+++ Use of Getters and Setters in main method.java
@@ -0,0 +1,24 @@
+class MyEmployee{
+  private int id;
+  private String name;
+  public String getName(){
+      return name;
+  }
+  public void setName(String n){
+  name =n;
+  }
+
+  public void setId(int i){
+        id = i;
+  }
+  public int getId(){
+      return id;
+  }
+}
+public class custom_class {
+    public static void main(String[] args) {
+        MyEmployee nitesh = new MyEmployee();
+        nitesh.setName("Nitesh Kumar");
+        System.out.println(nitesh.getName());
+    }
+}
