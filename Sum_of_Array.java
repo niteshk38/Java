05643f1b42cd555fb1bad1439c7bc763175e9fb9
Nitesh Kumar <niteshk38@gmail.com>
2024-01-22 07:14:26 +0530

    Create Sum_of_Array.java

---
+++ Sum_of_Array.java
@@ -0,0 +1,24 @@
+//Sum of all the numbers of an Array
+import java.util.Scanner;
+
+public class Main {
+    static int sum(int[] a) {
+        int sum = 0;
+        for(int i=0;i<=a.length-1;i++)
+            sum += a[i];
+        return sum;
+
+    }
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the count of Elements in the Array: ");
+        int n = s.nextInt();
+        System.out.println("Enter all the " + n + " elements of the Array: ");
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = s.nextInt();
+        }
+        System.out.println("Sum of All the Numbers in Array :"+ sum(a));
+    }
+}
